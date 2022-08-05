d177ab2be38870be575d4afcf78e9cc534b42fb7
vtnghia <nghia.vutruong@nttdata.com>
2022-08-05 15:42:28 +0700

    day 6

--- Blurry Loading/scripts.js
+++ Blurry Loading/scripts.js
@@ -14,5 +14,10 @@ function blurring() {
   }
 
   loadText.innerHTML = `${load}%`
-  bg.style.filter = `blur(${100-load}px)`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+  function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  } 
 }
